2c6f16478ff45f98cfb601a07a2fe014541e79fc
Igor Araujo <igor.dearaujo@live.com>
2020-06-28 22:49:32 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 require('./controllers/authController')(app)
 require('./controllers/projectController')(app)
 
-app.listen()
+app.listen(3000)
