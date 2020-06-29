659d1454567cb8d05a1bbc008b368eb8192cb379
Igor Araujo <igor.dearaujo@live.com>
2020-06-28 23:47:25 -0300

    add mongo url

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost/noderest', {
+mongoose.connect(
+    process.env.MONGO_URL,
+    {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
