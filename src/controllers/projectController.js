929294800b75766aa7f22eac98e9ff03052bb63a
Igor Araujo <igor.dearaujo@live.com>
2020-06-29 00:04:06 -0300

    test projectController

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -6,7 +6,8 @@ const router = express.Router()
 router.use(authMiddleware)
 
 router.get('/', (req, res) => {
-    res.send({ ok: true, user: req.userId })
+    res.send('ok')
+    // res.send({ ok: true, user: req.userId })
 })
 
 module.exports = app => app.use('/projects', router)
