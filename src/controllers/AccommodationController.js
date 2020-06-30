12503e1f3a7d28f67d335266f02fb079e16216b3
Igor Araujo <igor.dearaujo@live.com>
2020-06-29 23:25:41 -0300

    change Project name

---
+++ src/controllers/AccommodationController.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const authMiddleware = require('../middlewares/auth')
+
+const AccommodationOrder = require('../models/accommodation')
+
+const router = express.Router()
+
+router.use(authMiddleware)
+
+router.get('/', (req, res) => {
+    res.send({ ok: true, user: req.userId })
+})
+
+router.get('/:accommodationOrderId', async (req, res) => {
+    res.send({ user: req.userId })
+})
+
+router.post('/:accommodationOrder/create', async (req, res) => {
+    res.send({ user: req.userId })
+})
+
+router.put('/:accommodationOrderId', async (req, res) => {
+    res.send({ user: req.userId })
+})
+
+router.delete('/:accommodationOrderId', async (req, res) => {
+    res.send({ user: req.userId })
+})
+
+
+module.exports = app => app.use('/orders', router)
