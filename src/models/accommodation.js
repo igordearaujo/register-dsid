0274272283b9c73a6d3ad749b37eb03397229139
Igor Araujo <igor.dearaujo@live.com>
2020-06-29 23:25:51 -0300

    accomodationOrders

---
+++ src/models/accommodation.js
@@ -0,0 +1,43 @@
+const mongoose = require('../database')
+const bcrypt = require('bcryptjs')
+
+const AccommodationOrderSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    id: {
+        type: Number,
+        require: true,
+        unique: true,
+    },
+    pricePerNight: {
+        type: Number,
+        require: true
+    },
+    priceTotal: {
+        type: Number,
+        require: true,
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        require: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    }
+
+})
+
+AccommodationOrderSchema.pre('save', async function(next){
+    const hash = await bcrypt.hash(this.id, 10)
+    this.id = hash
+
+    next()
+})
+
+const AccommodationOrder = mongoose.model('AccommodationOrder', AccommodationOrderSchema)
+
+module.exports = AccommodationOrder
