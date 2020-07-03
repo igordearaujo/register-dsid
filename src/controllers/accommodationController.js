a59d7559a1cb5ab20c7b0552cdc1a977e28a9af2
Igor Araujo <igor.dearaujo@live.com>
2020-07-02 21:21:42 -0300

    trying again

--- src/controllers/accommodationController.js
+++ src/controllers/accommodationController.js
@@ -33,7 +33,7 @@ router.get('/:accommodationOrderId', async (req, res) => {
 
 router.get('/byuser/:userId', async (req, res) => {
     try {
-        const accommodationOrder = await AccommodationOrder.findOne(req.body).populate('AccommodationOrder')
+        const accommodationOrder = await AccommodationOrder.findOne(req.params.accommodationOrderId.userId).populate('AccommodationOrder')
 
 
         return res.send({ accommodationOrder })
